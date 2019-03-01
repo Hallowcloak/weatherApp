6b87d799130b49e9661295a83774d6de64cb7314
Mete <mete_tm@hotmail.com>
2019-03-01 20:08:04 +0300

    üçüncü gönderme

---
+++ app/src/main/java/tm/mete/weatherapp/Model/Clouds.java
@@ -0,0 +1,21 @@
+package tm.mete.weatherapp.Model;
+
+/**
+ * Created by Mete on 30.6.2017.
+ */
+
+public class Clouds {
+    private int all;
+
+    public Clouds(int all) {
+        this.all = all;
+    }
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
---
+++ app/src/main/java/tm/mete/weatherapp/Model/Rain.java
@@ -0,0 +1,8 @@
+package tm.mete.weatherapp.Model;
+
+/**
+ * Created by Mete on 30.6.2017.
+ */
+
+public class Rain {
+}
